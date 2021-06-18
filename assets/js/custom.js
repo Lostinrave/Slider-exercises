d2ea953489a821245c93ad11fd17a1531ce1aed9
Lostinrave <benaskalvinas@gmail.com>
2021-06-18 19:23:57 +0300

    failed commit

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            arrows: false,
+          });
+    });
+    

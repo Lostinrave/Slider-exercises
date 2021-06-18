80c127a13884f7a2b9b0b8dbd0e3b21b90946179
Lostinrave <benaskalvinas@gmail.com>
2021-06-18 20:24:46 +0300

    still not working am I stupid?

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,23 +2,17 @@ $(document).ready(function() {
     $('.custom-slider').slick({
             infinite: true,
             slidesToShow: 1,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
+          });
+          $('.custom-slider2').slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          });
+          $('.custom-slider3').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
           });
     });
     
-    $(document).ready(function() {
-      $('.custom-slider2').slick({
-              infinite: true,
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            });
-      });
-    
-      $(document).ready(function() {
-        $('.custom-slider3').slick({
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              });
-        });
-        

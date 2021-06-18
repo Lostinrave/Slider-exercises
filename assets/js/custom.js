de82343ad0fdbeb51ffd2e9af3deb156a0e4e3a5
Lostinrave <benaskalvinas@gmail.com>
2021-06-18 20:09:46 +0300

    klaida

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
-    $('.multiple-items').slick({
+    $('.custom-slider').slick({
             infinite: true,
-            slidesToShow: 6,
-            slidesToScroll: 6,
-            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 3,
           });
     });
     
+    $(document).ready(function() {
+      $('.custom-slider2').slick({
+              infinite: true,
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            });
+      });
+    
+      $(document).ready(function() {
+        $('.custom-slider3').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              });
+        });
+        
